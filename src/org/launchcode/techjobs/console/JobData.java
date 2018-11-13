71fb0a475a3793ec9c3f87f9f38d86106b1c7937
suha <suhasinichakka@gmail.com>
2018-11-13 12:24:38 -0600

    Added and finished print all jobs with search term commit

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -7,9 +7,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -40,12 +38,14 @@ public class JobData {
 
             if (!values.contains(aValue)) {
                 values.add(aValue);
+
             }
         }
 
         return values;
     }
 
+    //overloading
     public static ArrayList<HashMap<String, String>> findAll() {
 
         // load data, if not already loaded
@@ -57,12 +57,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of the field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -76,7 +76,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value)) {
                 jobs.add(row);
             }
         }
@@ -125,4 +125,29 @@ public class JobData {
         }
     }
 
+    public static ArrayList<HashMap<String,String>> findByValue(String searchValue) {
+
+        loadData();
+        ArrayList<HashMap<String, String>> printList = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            for (Map.Entry<String, String> col : row.entrySet()) {
+                if (col.getValue().toLowerCase().contains(searchValue.toLowerCase())) {
+                    printList.add(row);
+
+                    break;
+                }
+
+
+            }
+
+        }
+        return printList;
+    }
 }
+
+
+
+
+
+
--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -61,7 +62,8 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    //System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -110,7 +112,23 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        //System.out.println("printJobs is not implemented yet");
+        if(someJobs.size() == 0){
+            System.out.println("No results Found");
+        }else {
+            for (HashMap<String, String> row : someJobs) {
+                System.out.println("*******");
+                for (Map.Entry<String, String> colPrint : row.entrySet()) {
+                    System.out.println(colPrint.getKey() +  ":"   + colPrint.getValue());
+                }
+                System.out.println("*******");
+                System.out.println();
 
-        System.out.println("printJobs is not implemented yet");
-    }
+
+            }
+        }
+
+        }
+
+
 }
